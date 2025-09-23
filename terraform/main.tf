ccc7c6f8687bcecefd3bde0e6edec2ba87d2cfd2
Pooja Diwate <diwatepooja0110@gmail.com>
2025-09-23 20:41:10 +0530

    update in main.tf for vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,16 +18,16 @@ data "aws_internet_gateway" "existing" {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = data.aws_vpc.existing.id
+  vpc_id = data.aws_internet_gateway.existing.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = data.aws_internet_gateway.existing.id
   }
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+  subnet_id      = data.aws_subnet_ids.existing.ids[0]
   route_table_id = aws_route_table.public.id
 }
 
