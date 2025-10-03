39ba15e31abe2a691e8c079820c0f477f70be21e
Pooja Diwate <diwatepooja0110@gmail.com>
2025-10-03 23:30:17 +0530

    create cloudwatch settings

--- terraform-ecs-fargate/variables.tf
+++ terraform-ecs-fargate/variables.tf
@@ -8,10 +8,6 @@ variable "aws_account_id" {
   type        = string
 }
 
-variable "aws_region" { 
-  type = string 
-}
-
 variable "ecr_repo" {
   description = "ECR repository name"
   type        = string
@@ -23,7 +19,7 @@ variable "log_group_name" {
 }
 
 variable "service_name" { 
-  type = string, 
+  type = string 
   default = "strapi-service" 
 }
 
