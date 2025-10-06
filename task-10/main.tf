91d6e2f058cb63f5897753a161209f5e4fd4b044
Pooja Diwate <diwatepooja0110@gmail.com>
2025-10-07 01:29:55 +0530

    changes in main.tf file

--- task-10/main.tf
+++ task-10/main.tf
@@ -104,9 +104,10 @@ resource "aws_lb_target_group" "strapi_tg_green" {
   }
 }
 
-resource "aws_lb_listener" "alb_listener" {
+# Listener for production traffic (Blue)
+resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.strapi_alb.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
@@ -114,6 +115,19 @@ resource "aws_lb_listener" "alb_listener" {
     target_group_arn = aws_lb_target_group.strapi_tg_blue.arn
   }
 }
+
+# Listener for test traffic (Green)
+resource "aws_lb_listener" "test_listener" {
+  load_balancer_arn = aws_lb.strapi_alb.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.strapi_tg_green.arn
+  }
+}
+
 
 #########################################
 # ECS CLUSTER & TASK DEFINITION
@@ -233,7 +247,7 @@ resource "aws_codedeploy_deployment_group" "ecs_deploy_group" {
       }
 
       test_traffic_route {
-        listener_arns = [aws_lb_listener.alb_listener.arn]
+        listener_arns = [aws_lb_listener.test_listener.arn]
       }
 
       target_group {
@@ -253,38 +267,3 @@ resource "aws_codedeploy_deployment_group" "ecs_deploy_group" {
 
   deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
 }
-
-
-/*resource "aws_codedeploy_deployment_group" "ecs_deploy_group" {
-  app_name               = aws_codedeploy_app.ecs_app.name
-  deployment_group_name  = "strapi-bluegreen-dg"
-  service_role_arn       = "arn:aws:iam::145065858967:role/ecs-codedeploy-role"
-  deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
-
-  auto_rollback_configuration {
-    enabled = true
-    events  = ["DEPLOYMENT_FAILURE"]
-  }
-
-  blue_green_deployment_config {
-    terminate_blue_instances_on_deployment_success {
-      action = "TERMINATE"
-      termination_wait_time_in_minutes = 1
-    }
-  }
-
-  ecs_service {
-    cluster_name = aws_ecs_cluster.strapi_cluster.name
-    service_name = aws_ecs_service.strapi_service.name
-  }
-
-  load_balancer_info {
-    target_group_pair_info {
-      prod_traffic_route {
-        listener_arns = [aws_lb_listener.alb_listener.arn]
-      }
-
-      target_group {
-        name = aws_lb_target_group.strapi_tg_blue.name
-      }
-*/
